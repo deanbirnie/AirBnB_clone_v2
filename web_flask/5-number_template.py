6b30c10447aaec99f52ab742e5642c0d52e966f2
deanbirnie <deanicdl@gmail.com>
2023-09-25 08:57:22 -0700

    Task 5 - Let Flask handle checking integer in URL

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,10 +44,9 @@ def isNumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numberTemplate(n=None):
+def numberTemplate(n):
     """Display an HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
