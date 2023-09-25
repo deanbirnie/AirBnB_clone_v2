db06a5a8203d232eb3cdd3f64bc37bca4c365ef9
deanbirnie <deanicdl@gmail.com>
2023-09-25 06:03:51 -0700

    Task 4 - Route that takes an integer variable and checks if it is an integer returning a string

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+Start a Flask web application running
+on 0.0.0.0 using port 5000 with routes.
+"""
+
+from flask import Flask
+
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Default route that returns a simple string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cVariable(text):
+    """Display 'C' followed by value of text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonVariable(text="is cool"):
+    """Display 'Python' followed by the value of text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isNumber(n):
+    """Display n followed by a string"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
