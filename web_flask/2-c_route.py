c24c781d0c237cd95dec67502a25f16ab3a1d6bb
deanbirnie <deanicdl@gmail.com>
2023-09-25 05:15:43 -0700

    Task 2 - Route with variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Start a Flask web application running
+on 0.0.0.0 using port 5000 with routes.
+"""
+
+from flask import Flask
+
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Default route that returns a simple string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cVariable(text):
+    """Display 'C' followed by value of text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
